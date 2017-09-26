fa68484e7ea8bed70052d2accf2e46d1987757c1
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2017-09-26 15:55:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Base122 Encoder and Decoder
+
+WIP
+
+## Build
+
+### Jar Sign
+
+If you want to jar sign you need to provide a file `keystore.jks` in the
+root folder with the correct credentials set in environment variables (
+`OPENSOURCE_PROJECTS_KS_PW` and `OPENSOURCE_PROJECTS_KEY_PW`); alias is
+set as `pfopensource`.
+
+If you want to skip jar signing just change the skip configuration in the
+`pom.xml` jar sign plugin to true:
+
+    <skip>true</skip>
+
+### Build with Maven
+
+Use maven (3.1+) to create a jar including all dependencies
+
+    mvn clean install
+
+## Tech Stack
+
+* Java 8
+* Maven
+* rxjava2, apache-commons-codec, apache-commons-cli, Retrofit 2
+* Proguard, Launch4j, Jar Signing
+
+# Credits
+
+* HMAC_DRBG implementation derived from [google/rappor](https://github.com/google/rappor)
+* Icon made by [Maxim Basinski](https://www.flaticon.com/authors/maxim-basinski) from www.flaticon.com
+
+# License
+
+Copyright 2017 Patrick Favre-Bulle
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/main/java/at/favre/encoding/Base122.java
@@ -0,0 +1,47 @@
+package at.favre.encoding;
+
+public class Base122 {
+
+    static class Encoder {
+        private int curIndex = 0;
+        private int curBit = 0;
+        private byte[] rawData;
+
+        Encoder(byte[] rawData) {
+            this.rawData = rawData;
+        }
+
+        String encode() {
+            return null;
+        }
+
+        byte get7Bit() {
+            if (curIndex >= rawData.length)
+                throw new IllegalArgumentException("current index is greater than data length");
+            
+            // Shift, mask, unshift to get first part.
+            byte firstByte = rawData[curIndex];
+            byte firstPart = (byte) (((0b11111110 >>> curBit) & firstByte) << curBit);
+            // Align it to a seven bit chunk.
+            firstPart >>= 1;
+            // Check if we need to go to the next byte for more bits.
+            curBit += 7;
+            if (curBit < 8) return firstPart; // Do not need next byte.
+            curBit -= 8;
+            curIndex++;
+            // Now we want bits [0..curBit] of the next byte if it exists.
+            if (curIndex >= rawData.length) return firstPart;
+            byte secondByte = rawData[curIndex];
+            byte secondPart = (byte) (((0xFF00 >>> curBit) & secondByte) & 0xFF);
+            // Align it.
+            secondPart >>= 8 - curBit;
+            return (byte) (firstPart | secondPart);
+        }
+    }
+
+
+    public String encode(byte[] data) {
+        return new Encoder(data).encode();
+    }
+
+}
